563db5532bacbb03f5976b977316949e489f19ec
Emilie <ehn1994@gmail.com>
2016-11-21 10:52:46 +0100

    Design og angular påbegyndt

---
+++ src/main/webapp/angular.js
@@ -0,0 +1,12 @@
+var app = angular.module('myApp', []);
+
+app.controller("myCtrl", function($scope){
+
+    
+});
+
+app.filter('dateFilter', function(){
+    return function(){
+        
+    };
+});
