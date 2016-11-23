c2922533b8ba498b0a8f2be2122cc54bb922bf58
Emilie <ehn1994@gmail.com>
2016-11-23 09:35:47 +0100

    Test oprettet. 2 bestået

--- src/main/webapp/angular.js
+++ src/main/webapp/angular.js
@@ -5,9 +5,12 @@ var app = angular.module('myApp', []);
 app.controller("myCtrl", function($scope, $http){
     $scope.test = "test";
   $http.get('http://airline-plaul.rhcloud.com/api/flightinfo/SXF/2017-01-03T00:00:00.000Z/2').
-        then(function(response) {
-            $scope.data = response.data;
-            $scope.flight = $scope.data.flights;
+        success(function(response) {
+            $scope.data = response;
+            console.log("Det gik godt");
+        }).error(function(response){
+            
+            $scope.status = response.statusText;
         });
 });
 
--- src/test/java/com/mycompany/ca4/DummyAirlineRestIntegrationTest.java
+++ src/test/java/com/mycompany/ca4/DummyAirlineRestIntegrationTest.java
@@ -5,10 +5,71 @@
  */
 package com.mycompany.ca4;
 
+import static io.restassured.RestAssured.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import static org.hamcrest.Matchers.*;
+import org.junit.Before;
+import org.junit.Test;
+
 /**
  *
  * @author ehn19
  */
 public class DummyAirlineRestIntegrationTest {
+
+    @Before
+    public void setUp() {
+
+    }
+
+    @Test
+    public void webpageIsRunning() {
+        given().when().get("http://localhost:8080/Semesterprojekt").then().statusCode(200);
+    }
+
+    @Test
+    public void webpageIsNotRunning2() {
+        given().when().get("http://localhost:8080/SemesterprojektFail").then().statusCode(404);
+       
+    }
     
+//    @Test
+//    public void getFlightWithFromDateTickets()
+//    {
+//        expect().
+//                body("get(0).flightID", equalTo("2215-1483448400000")).
+//                when().
+//                get("/date/2017-01-03T08:00:00.000Z/tickets/2");
+//        
+//    }
+
+//    @Test
+//    public void getFlightWithFromDateTickets2()
+//    {
+//        when().
+//                get("/date/{date}/tickets/{tickets}",2017-01-03,2).
+//        then().statusCode(200).
+//                body("flight.date", equalTo(2017-01-03),"flight.numberOfSeats", equalTo(2));
+//    }
+//    
+//    @Test
+//    public Reservation postReservationWithFlightIdPassengers()
+//    {
+//      List<Passenger> passengers = new ArrayList();  
+//      Collection<Passenger> passengers;
+//    } 
+    
+    @Test
+    public void statusCode404()
+    {
+        given().when().get("/date/{date}/tickets/{tickets}\",2080-01-03,2").then().statusCode(404);
+    }
+    
+    @Test
+    public void statusCode200()
+    {
+        given().when().get("/date/{date}/tickets/{tickets}\",2080-01-03,2").then().statusCode(404);
+    }
 }
