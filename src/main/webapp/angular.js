b7e4a449592584ac2795a170d5d47c737c7edf15
Emilie <ehn1994@gmail.com>
2016-11-24 11:16:51 +0100

    Fix fix. Fly ud fra dato

--- src/main/webapp/angular.js
+++ src/main/webapp/angular.js
@@ -7,7 +7,7 @@ app.controller("myCtrl", function ($scope, $http) {
     $scope.numSeats;
     $scope.date;
     $scope.airport;
-
+    $scope.destination;
 
     $scope.details = function () {
         console.log($scope.date);
@@ -17,27 +17,30 @@ app.controller("myCtrl", function ($scope, $http) {
         var stringy = (daty.getDate() == 1 ? daty.getUTCFullYear() + 1 : daty.getFullYear()) + "-" +
                 ((daty.getDate() == 1 ? daty.getUTCMonth() + 2 : daty.getUTCMonth() + 1) < 10 ?
                         "0" + (daty.getDate() == 1 ? daty.getUTCMonth() + 2 : daty.getUTCMonth() + 1) :
+                        ((daty.getDate() == 1 ? daty.getUTCMonth() + 2 : daty.getUTCMonth() + 1)) == 13 ? 1 : 
                         (daty.getDate() == 1 ? daty.getUTCMonth() + 2 : daty.getUTCMonth() + 1))
                 + "-" + (daty.getDate() < 10 ? "0" + daty.getDate() : daty.getDate()) + "T" + "00" + ":" + "00" + ":" + "00.000" + "Z";
         console.log("Stringy: " + stringy);
         console.log("Airport: " + $scope.airport);
-        var url = 'http://airline-plaul.rhcloud.com/api/flightinfo/' + $scope.airport + "/" + stringy + "/" + $scope.numSeats;
+        var dest = (angular.isUndefined($scope.destination) ? "" : 
+                ($scope.destination == "" ? "":
+                "/" + $scope.destination));
+        var url = 'http://airline-plaul.rhcloud.com/api/flightinfo/' + $scope.airport + dest + "/" + stringy + "/" + $scope.numSeats;
         console.log(url);
+        $http.get(url).
+                success(function (response) {
+                    $scope.data = response;
+                    console.log("Det gik godt");
+                }).error(function (response) {
+            alert(response.message);
+        });
     };
 
-    $http.get('http://airline-plaul.rhcloud.com/api/flightinfo/SXF/2017-01-03T00:00:00.000Z/2').
-            success(function (response) {
-                $scope.data = response;
-                console.log("Det gik godt");
-            }).error(function (response) {
-
-        $scope.status = response.statusText;
-    });
 
 
 });
 
-app.filter('dateFilter', function () {
+app.filter('priceFilter', function () {
     return function () {
 
     };
