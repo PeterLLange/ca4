55deecd919d5f0629a1afda1d1291ed01c4408b8
EmilUlrikChristoffersen <emilulrikchristoffersen@outlook.dk>
2016-11-30 09:42:50 +0100

    deleted_entity

--- src/main/java/httpcall/httpcall.java
+++ src/main/java/httpcall/httpcall.java
@@ -6,7 +6,7 @@
 package httpcall;
 
 import com.google.gson.Gson;
-import Entity.Airline;
+import com.google.gson.JsonObject;
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.InputStreamReader;
@@ -55,21 +55,60 @@ public class httpcall {
         return response.toString();
     }
     // HTTP GET request
-    private static String sendGet() throws Exception {
+//    private static String sendGet() throws Exception {
+//
+//        String url = "http://www.google.com/search?q=mkyong";
+//
+//        URL obj = new URL(url);
+//        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+//
+//        // optional default is GET
+//        con.setRequestMethod("GET");
+//
+//        //add request header
+//        con.setRequestProperty("User-Agent", USER_AGENT);
+//
+//        int responseCode = con.getResponseCode();
+//        System.out.println("\nSending 'GET' request to URL : " + url);
+//        System.out.println("Response Code : " + responseCode);
+//
+//        BufferedReader in = new BufferedReader(
+//                new InputStreamReader(con.getInputStream()));
+//        String inputLine;
+//        StringBuffer response = new StringBuffer();
+//
+//        while ((inputLine = in.readLine()) != null) {
+//            response.append(inputLine);
+//        }
+//        in.close();
+//
+//        //print result
+//        System.out.println(response.toString());
+//        return response.toString();
+//    }
+    // HTTP POST request
 
-        String url = "http://www.google.com/search?q=mkyong";
+    private static String sendPost(String paremeter, JsonObject flight) throws Exception {
 
+        String url = "http://airline-plaul.rhcloud.com/"+paremeter;
         URL obj = new URL(url);
-        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+        HttpsURLConnection con = (HttpsURLConnection) obj.openConnection();
 
-        // optional default is GET
-        con.setRequestMethod("GET");
-
-        //add request header
+        //add reuqest header
+        con.setRequestMethod("POST");
         con.setRequestProperty("User-Agent", USER_AGENT);
+        con.setRequestProperty("Accept-Language", "en-US,en;q=0.5");
 
+        // Send post request
+        con.setDoOutput(true);
+        DataOutputStream wr = new DataOutputStream(con.getOutputStream());
+        wr.writeBytes(flight.getAsString());
+        wr.flush();
+        wr.close();
+        flight.getAsJsonObject().getAsJsonObject().getAsJsonObject().getAsJsonObject().getAsJsonObject().getAsJsonObject().getAsJsonObject().getAsJsonObject().getAsJsonObject().getAsJsonObject();
         int responseCode = con.getResponseCode();
-        System.out.println("\nSending 'GET' request to URL : " + url);
+        System.out.println("\nSending 'POST' request to URL : " + url);
+        System.out.println("Post parameters : " + flight.getAsString());
         System.out.println("Response Code : " + responseCode);
 
         BufferedReader in = new BufferedReader(
@@ -85,46 +124,5 @@ public class httpcall {
         //print result
         System.out.println(response.toString());
         return response.toString();
-    }
-    // HTTP POST request
-
-    private static Airline sendPost() throws Exception {
-
-        String url = "https://selfsolve.apple.com/wcResults.do";
-        URL obj = new URL(url);
-        HttpsURLConnection con = (HttpsURLConnection) obj.openConnection();
-
-        //add reuqest header
-        con.setRequestMethod("POST");
-        con.setRequestProperty("User-Agent", USER_AGENT);
-        con.setRequestProperty("Accept-Language", "en-US,en;q=0.5");
-
-        String urlParameters = "sn=C02G8416DRJM&cn=&locale=&caller=&num=12345";
-
-        // Send post request
-        con.setDoOutput(true);
-        DataOutputStream wr = new DataOutputStream(con.getOutputStream());
-        wr.writeBytes(urlParameters);
-        wr.flush();
-        wr.close();
-
-        int responseCode = con.getResponseCode();
-        System.out.println("\nSending 'POST' request to URL : " + url);
-        System.out.println("Post parameters : " + urlParameters);
-        System.out.println("Response Code : " + responseCode);
-
-        BufferedReader in = new BufferedReader(
-                new InputStreamReader(con.getInputStream()));
-        String inputLine;
-        StringBuffer response = new StringBuffer();
-
-        while ((inputLine = in.readLine()) != null) {
-            response.append(inputLine);
-        }
-        in.close();
-
-        //print result
-        System.out.println(response.toString());
-        return new Gson().fromJson(response.toString(), Airline.class);
     }
 }
